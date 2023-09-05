7b9f252a96a064018609f340659e3cfcd89000ba
Lee-Jiseung <ljss98@naver.com>
2023-09-05 13:28:04 +0900

    :tada: Init: Initialize SpringBoot project

---
+++ backend/src/main/java/com/dohit/huick/HuickApplication.java
@@ -0,0 +1,13 @@
+package com.dohit.huick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuickApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/dohit/huick/HuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dohit.huick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
