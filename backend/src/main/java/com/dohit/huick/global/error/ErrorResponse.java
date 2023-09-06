11b740b52b2db19b09822981ebf6d014e3c7b929
fluffymn <mineezm9@gmail.com>
2023-09-06 12:46:39 +0900

    ✨ Feat: Implement a function that generates an error message of log

--- backend/src/main/java/com/dohit/huick/global/error/ErrorResponse.java
+++ backend/src/main/java/com/dohit/huick/global/error/ErrorResponse.java
@@ -1,5 +1,10 @@
 package com.dohit.huick.global.error;
 
+import java.util.List;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,5 +19,23 @@ public class ErrorResponse {
 			.errorCode(errorCode)
 			.errorMessage(errorMessage)
 			.build();
+	}
+
+	private static String createErrorMessage(BindingResult bindingResult) {
+		StringBuilder sb = new StringBuilder();
+		boolean isFirst = true;
+		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+		for (FieldError fieldError : fieldErrors) {
+			if (!isFirst) {
+				sb.append(", ");
+			} else {
+				isFirst = false;
+			}
+			sb.append("[");
+			sb.append(fieldError.getField());
+			sb.append("]");
+			sb.append(fieldError.getDefaultMessage());
+		}
+		return sb.toString();
 	}
 }
