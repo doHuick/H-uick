86e54ac1647adc9b20e25c07b01387e64b320dea
fluffymn <mineezm9@gmail.com>
2023-09-06 12:45:32 +0900

    ✨ Feat: Implement the function creating an error response with error codes and error messages

--- backend/src/main/java/com/dohit/huick/global/error/ErrorResponse.java
+++ backend/src/main/java/com/dohit/huick/global/error/ErrorResponse.java
@@ -8,4 +8,11 @@ import lombok.Getter;
 public class ErrorResponse {
 	private String errorCode;
 	private String errorMessage;
+
+	public static ErrorResponse of(String errorCode, String errorMessage) {
+		return ErrorResponse.builder()
+			.errorCode(errorCode)
+			.errorMessage(errorMessage)
+			.build();
+	}
 }
