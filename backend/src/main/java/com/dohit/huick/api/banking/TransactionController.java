a184eaa61a34bfca92c8f391a4b03afe22fb8f43
fluffymn <mineezm9@gmail.com>
2023-09-08 15:47:58 +0900

    ✨ Feat: Implement api GET `/banking/transactions/{userId}` that return transaction data by user id

--- backend/src/main/java/com/dohit/huick/api/banking/TransactionController.java
+++ backend/src/main/java/com/dohit/huick/api/banking/TransactionController.java
@@ -1,6 +1,11 @@
 package com.dohit.huick.api.banking;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +24,12 @@ public class TransactionController {
 	public ResponseEntity<Void> transferMoney(@RequestBody TransactionApiDto.Request request) {
 		bankingService.transferMoney(TransactionDto.from(request));
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<List<TransactionApiDto.Response>> getTransactionsByUserId(@PathVariable Long userId) {
+		return ResponseEntity.ok(bankingService.getTransactionsByUserId(userId).stream().map(
+			TransactionApiDto.Response::from).collect(
+			Collectors.toList()));
 	}
 }
