25c0fc5298ae6a0fb32abe9f92b216c52c1c5c56
Lee-Jiseung <ljss98@naver.com>
2023-09-08 13:30:09 +0900

    ✨ Feat: Implement RefreshTokenRepository

---
+++ backend/src/main/java/com/dohit/huick/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.dohit.huick.domain.auth.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dohit.huick.domain.auth.entity.RefreshToken;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+	Optional<RefreshToken> findByUserId(Long userId);
+
+	Optional<RefreshToken> findByUserIdAndRefreshToken(Long userId, String refreshToken);
+}
