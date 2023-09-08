ff09620fc3fdd1d4e50b966964f1bb6a1b4be033
Lee-Jiseung <ljss98@naver.com>
2023-09-08 13:50:52 +0900

    ✨ Feat: Implement AppProperties

--- backend/src/main/java/com/dohit/huick/HuickApplication.java
+++ backend/src/main/java/com/dohit/huick/HuickApplication.java
@@ -2,12 +2,16 @@ package com.dohit.huick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.dohit.huick.global.property.AppProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({AppProperties.class})
 public class HuickApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HuickApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HuickApplication.class, args);
+	}
 
 }
