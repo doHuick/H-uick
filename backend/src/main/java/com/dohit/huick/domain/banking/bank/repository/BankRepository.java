3b9064961347c35f88f2b71f5d6e232289b9fb39
fluffymn <mineezm9@gmail.com>
2023-09-08 15:12:02 +0900

    ✨ Feat: Implement a query method that find bank data by bank code

--- backend/src/main/java/com/dohit/huick/domain/banking/bank/repository/BankRepository.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/bank/repository/BankRepository.java
@@ -8,4 +8,6 @@ import com.dohit.huick.domain.banking.bank.entity.Bank;
 
 public interface BankRepository extends JpaRepository<Bank, Long> {
 	Optional<Bank> findByBankId(Long bankId);
+
+	Optional<Bank> findByBankCode(String bankCode);
 }
