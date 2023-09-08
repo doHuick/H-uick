a57e43b91299b5c6f0b498af0bc5927e50f257fe
fluffymn <mineezm9@gmail.com>
2023-09-08 15:34:13 +0900

    ✨ Feat: Implement a transaction service method that get transaction data by user id

--- backend/src/main/java/com/dohit/huick/domain/banking/transaction/service/TransactionService.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/transaction/service/TransactionService.java
@@ -1,5 +1,8 @@
 package com.dohit.huick.domain.banking.transaction.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +21,9 @@ public class TransactionService {
 
 	public void createTransaction(TransactionDto transactionDto) {
 		transactionRepository.save(Transaction.from(transactionDto));
+	}
+
+	public List<TransactionDto> getTransactionsByUserId(Long userId) {
+		return transactionRepository.findByUserId(userId).stream().map(TransactionDto::from).collect(Collectors.toList());
 	}
 }
