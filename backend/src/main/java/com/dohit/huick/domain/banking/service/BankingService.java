abbcfe6c0c302b991ffea07eec37b3c42d771f9f
fluffymn <mineezm9@gmail.com>
2023-09-08 15:38:04 +0900

    ✨ Feat: Implement a banking service method that get random money when user sign in

--- backend/src/main/java/com/dohit/huick/domain/banking/service/BankingService.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/service/BankingService.java
@@ -1,10 +1,12 @@
 package com.dohit.huick.domain.banking.service;
 
+import java.security.SecureRandom;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dohit.huick.domain.banking.account.service.AccountService;
-import com.dohit.huick.domain.banking.bank.service.BankService;
+import com.dohit.huick.domain.banking.transaction.dto.TransactionDto;
 import com.dohit.huick.domain.banking.transaction.service.TransactionService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,9 +17,26 @@ import lombok.RequiredArgsConstructor;
 public class BankingService {
 
 	private final AccountService accountService;
+	private final TransactionService transactionService;
 
 	public void createAccount(Long userId) {
 		accountService.createAccount(userId);
 	}
 
+	public void transferRandomMoney(String accountNumber) {
+		// 랜덤한 잔액 생성하기
+		SecureRandom secureRandom = new SecureRandom();
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(secureRandom.nextInt(900000) + 100000);
+		int random = secureRandom.nextInt(3) + 3;
+		stringBuilder.append("0".repeat(random));
+		Long randomMoney = Long.parseLong(stringBuilder.toString());
+
+		// 어카운트 넘버로 어카운트를 찾아와서 잔액을 변경함
+		accountService.updateBalance(accountNumber, randomMoney);
+
+		// 트랜잭션 데이터를 생성
+		TransactionDto transactionDto = TransactionDto.of("100408000000", accountNumber, randomMoney);
+		transactionService.createTransaction(transactionDto);
+	}
 }
