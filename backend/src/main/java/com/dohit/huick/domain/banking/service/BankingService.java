6018bdcb2554349e99bfb1f79aef1e98dcbb5e99
fluffymn <mineezm9@gmail.com>
2023-09-08 15:42:01 +0900

    ✨ Feat: Implement a banking service method that transfer money

--- backend/src/main/java/com/dohit/huick/domain/banking/service/BankingService.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/service/BankingService.java
@@ -10,6 +10,8 @@ import com.dohit.huick.domain.banking.account.service.AccountService;
 import com.dohit.huick.domain.banking.bank.service.BankService;
 import com.dohit.huick.domain.banking.transaction.dto.TransactionDto;
 import com.dohit.huick.domain.banking.transaction.service.TransactionService;
+import com.dohit.huick.global.error.ErrorCode;
+import com.dohit.huick.global.error.exception.BankingException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -46,5 +48,23 @@ public class BankingService {
 	public AccountDto getAccountByUserId(Long userId) {
 		AccountDto account = accountService.getAccountsByUserId(userId).get(0);
 		return AccountDto.of(account, bankService.getBankByBankCode(account.getBankCode()));
+	}
+
+	public void transferMoney(TransactionDto transactionDto) {
+		// from 계좌 가져옴
+		AccountDto senderAccountDto = accountService.getAccountByAccountNumber(transactionDto.getSenderAccountNumber());
+		if(senderAccountDto.getBalance() < transactionDto.getAmount()) {
+			throw new BankingException(ErrorCode.NOT_ENOUGH_MONEY);
+		}
+
+		// from 계좌에서 돈을 뺌
+		accountService.updateBalance(senderAccountDto.getAccountNumber(), -transactionDto.getAmount());
+
+		// to 계좌  가져와서 돈을 넣음
+		AccountDto receiverAccountDto = accountService.getAccountByAccountNumber(transactionDto.getReceiverAccountNumber());
+		accountService.updateBalance(receiverAccountDto.getAccountNumber(), transactionDto.getAmount());
+
+		// 트랜잭션 데이터를 생성함
+		transactionService.createTransaction(transactionDto);
 	}
 }
