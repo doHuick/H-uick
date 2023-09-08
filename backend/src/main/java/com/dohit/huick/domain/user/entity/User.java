79285478e59d5f78319aaa6da342d1b3de7a72bb
Lee-Jiseung <ljss98@naver.com>
2023-09-08 13:10:09 +0900

    🔥 Remove: Remove NotNull from User

--- backend/src/main/java/com/dohit/huick/domain/user/entity/User.java
+++ backend/src/main/java/com/dohit/huick/domain/user/entity/User.java
@@ -10,12 +10,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.dohit.huick.domain.auth.constant.Role;
 import com.dohit.huick.domain.auth.constant.SocialType;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -29,60 +27,49 @@ import lombok.Setter;
 @Table(name = "User")
 public class User {
 
-	@JsonIgnore
 	@Id
 	@Column(name = "user_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
 
 	@Column(name = "wallet_address", length = 512, unique = true)
-	@NotNull
 	@Size(max = 512)
 	private String walletAddress;
 
 	@Column(name = "social_type", length = 20)
 	@Enumerated(EnumType.STRING)
-	@NotNull
 	private SocialType socialType;
 
 	@Column(name = "social_id", length = 512, unique = true)
-	@NotNull
 	@Size(max = 512)
 	private String socialId;
 
 	@Column(name = "created_time")
-	@NotNull
 	private LocalDateTime createdTime;
 
 	@Column(name = "role", length = 20)
 	@Enumerated(EnumType.STRING)
-	@NotNull
 	private Role role;
 
 	@Column(name = "withdrawal_time")
 	private LocalDateTime withdrawalTime;
 
 	@Column(name = "signature_url")
-	@NotNull
 	private String signatureUrl;
 
 	@Column(name = "rrn", length = 13)
-	@NotNull
 	@Size(min = 13, max = 13)
 	private String rrn;
 
 	@Column(name = "address", length = 100)
-	@NotNull
 	@Size(max = 100)
 	private String address;
 
 	@Column(name = "name", length = 20)
-	@NotNull
 	@Size(max = 20)
 	private String name;
 
 	@Column(name = "issue_date")
-	@NotNull
 	private LocalDateTime issueDate;
 
 	@Builder
@@ -101,5 +88,5 @@ public class User {
 		this.name = name;
 		this.issueDate = issueDate;
 	}
-	
+
 }
