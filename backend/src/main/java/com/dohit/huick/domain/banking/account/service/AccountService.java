060887a1ae7230ae26ac089818ffde32fed53df7
fluffymn <mineezm9@gmail.com>
2023-09-08 14:54:45 +0900

    ✨ Feat: Implement a service method that get accounts by user id

---
+++ backend/src/main/java/com/dohit/huick/domain/banking/account/service/AccountService.java
@@ -0,0 +1,32 @@
+package com.dohit.huick.domain.banking.account.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dohit.huick.domain.banking.account.dto.AccountDto;
+import com.dohit.huick.domain.banking.account.entity.Account;
+import com.dohit.huick.domain.banking.account.repository.AccountRepository;
+import com.dohit.huick.global.error.ErrorCode;
+import com.dohit.huick.global.error.exception.BusinessException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AccountService {
+
+	private final AccountRepository accountRepository;
+
+	public List<AccountDto> getAccountsByUserId(Long userId){
+		List<Account> accounts = accountRepository.findByUserId(userId);
+		if(accounts.size() == 0) {
+			throw new BusinessException(ErrorCode.NO_ACCOUNT_EXIST);
+		}
+
+		return accounts.stream().map(AccountDto::from).collect(Collectors.toList());
+	}
+}
