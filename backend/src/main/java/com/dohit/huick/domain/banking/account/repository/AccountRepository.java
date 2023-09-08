8d2be4c87a8d89f1460292823465c027955f3eba
fluffymn <mineezm9@gmail.com>
2023-09-08 14:39:05 +0900

    ✨ Feat: Implement a query method that find accounts by user id

--- backend/src/main/java/com/dohit/huick/domain/banking/account/repository/AccountRepository.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/account/repository/AccountRepository.java
@@ -1,8 +1,11 @@
 package com.dohit.huick.domain.banking.account.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dohit.huick.domain.banking.account.entity.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+	List<Account> findByUserId(Long userId);
 }
