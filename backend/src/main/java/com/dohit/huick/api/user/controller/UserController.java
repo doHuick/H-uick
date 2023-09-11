2df28a78637d7f5e18a71a753c4f1e78b7538a56
Lee-Jiseung <ljss98@naver.com>
2023-09-11 11:39:50 +0900

    ✨ Feat: Implement a withdrawal method in UserController

--- backend/src/main/java/com/dohit/huick/api/user/controller/UserController.java
+++ backend/src/main/java/com/dohit/huick/api/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.dohit.huick.api.user.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,14 @@ public class UserController {
 	@PostMapping("/rrn")
 	public ResponseEntity<?> signup(@RequestBody UserApiDto.Request request) {
 		userService.signup(UserDto.from(request));
-		
+
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("/me")
+	public ResponseEntity<Void> withdraw() {
+		userService.withdraw();
+
 		return ResponseEntity.ok().build();
 	}
 
