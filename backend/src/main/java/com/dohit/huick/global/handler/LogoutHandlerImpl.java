f989d4349b52f5fcd09741ef65dd74081787dbaf
Lee-Jiseung <ljss98@naver.com>
2023-09-11 15:55:14 +0900

    ✏️ Fix: Add a parameter of logout method

--- backend/src/main/java/com/dohit/huick/global/handler/LogoutHandlerImpl.java
+++ backend/src/main/java/com/dohit/huick/global/handler/LogoutHandlerImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dohit.huick.domain.auth.service.AuthService;
+import com.dohit.huick.global.util.HeaderUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,6 @@ public class LogoutHandlerImpl implements LogoutHandler {
 
 	@Override
 	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-		authService.logout();
+		authService.logout(HeaderUtil.getAccessToken(request));
 	}
 }
