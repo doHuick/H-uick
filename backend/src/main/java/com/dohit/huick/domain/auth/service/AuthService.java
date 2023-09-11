b2e52142ec1266fbfe6f91b9790f30df93f593d7
Lee-Jiseung <ljss98@naver.com>
2023-09-11 15:53:33 +0900

    ✏️ Fix: Change how to get user information

--- backend/src/main/java/com/dohit/huick/domain/auth/service/AuthService.java
+++ backend/src/main/java/com/dohit/huick/domain/auth/service/AuthService.java
@@ -1,12 +1,13 @@
 package com.dohit.huick.domain.auth.service;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dohit.huick.domain.auth.repository.RefreshTokenRepository;
 import com.dohit.huick.global.error.ErrorCode;
 import com.dohit.huick.global.error.exception.BusinessException;
+import com.dohit.huick.global.token.AuthToken;
+import com.dohit.huick.global.token.AuthTokenProvider;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,14 +19,14 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthService {
 
 	private final RefreshTokenRepository refreshTokenRepository;
+	private final AuthTokenProvider authTokenProvider;
 
-	public void logout() throws BusinessException {
-		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+	public void logout(String accessToken) throws BusinessException {
+		AuthToken authToken = authTokenProvider.convertAuthToken(accessToken);
+		String name = authTokenProvider.getAuthentication(authToken).getName();
 		if (name == null) {
 			throw new BusinessException(ErrorCode.NOT_EXIST_USER);
 		}
-
-		Long userId = Long.valueOf(name);
-		refreshTokenRepository.deleteByUserId(userId);
+		refreshTokenRepository.deleteByUserId(Long.valueOf(name));
 	}
 }
