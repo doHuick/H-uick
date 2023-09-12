b91dc66ce56da8884bb199eae39f6c7ba448f2bc
fluffymn <mineezm9@gmail.com>
2023-09-12 15:15:26 +0900

    ✨ Feat: Implement a api POST `/contracts` that create contract

---
+++ backend/src/main/java/com/dohit/huick/api/contract/controller/ContractController.java
@@ -0,0 +1,26 @@
+package com.dohit.huick.api.contract.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dohit.huick.api.contract.dto.ContractApiDto;
+import com.dohit.huick.domain.contract.dto.ContractDto;
+import com.dohit.huick.domain.contract.service.ContractService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/contracts")
+@RequiredArgsConstructor
+public class ContractController {
+	private final ContractService contractService;
+
+	@PostMapping
+	public ResponseEntity<Void> createContract(@RequestBody ContractApiDto.Request request) {
+		contractService.createContract(ContractDto.from(request));
+		return ResponseEntity.ok().build();
+	}
+}
