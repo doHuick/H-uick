5380fefc30a1d7f3da03ef4acb46b302d51d2033
Lee-Jiseung <ljss98@naver.com>
2023-09-12 15:40:47 +0900

    ✏️ Fix: Change RefreshTokenRepository to use redis

--- backend/src/main/java/com/dohit/huick/domain/auth/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/dohit/huick/domain/auth/repository/RefreshTokenRepository.java
@@ -1,15 +1,36 @@
 package com.dohit.huick.domain.auth.repository;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
 import com.dohit.huick.domain.auth.entity.RefreshToken;
 
-@Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-	Optional<RefreshToken> findByUserId(Long userId);
+import lombok.RequiredArgsConstructor;
 
-	Optional<RefreshToken> findByUserIdAndRefreshToken(Long userId, String refreshToken);
+@Repository
+@RequiredArgsConstructor
+public class RefreshTokenRepository {
+	private final RedisTemplate redisTemplate;
+
+	public void save(RefreshToken refreshToken) {
+		System.out.println(refreshToken);
+		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+		valueOperations.set(String.valueOf(refreshToken.getUserId()), refreshToken.getRefreshToken());
+		redisTemplate.expire(refreshToken.getRefreshToken(), 7, TimeUnit.DAYS);
+	}
+
+	public Optional<RefreshToken> findByUserId(Long userId) {
+		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+		RefreshToken refreshToken = RefreshToken.of(userId, valueOperations.get(String.valueOf(userId)));
+
+		if (Objects.isNull(refreshToken)) {
+			return Optional.empty();
+		}
+		return Optional.of(refreshToken);
+	}
 }
