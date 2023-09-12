74673aa03e31a10d07649d2ef9937029e01b53b9
Lee-Jiseung <ljss98@naver.com>
2023-09-12 15:14:50 +0900

    :pencil2: Fix: Change the RefreshToken entity to use redis

--- backend/src/main/java/com/dohit/huick/domain/auth/entity/RefreshToken.java
+++ backend/src/main/java/com/dohit/huick/domain/auth/entity/RefreshToken.java
@@ -1,39 +1,19 @@
 package com.dohit.huick.domain.auth.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import org.springframework.data.annotation.Id;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "RefreshToken")
+@ToString
 public class RefreshToken {
 
 	@Id
-	@Column(name = "refresh_token_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long refreshTokenId;
-
-	@Column(name = "user_id", unique = true)
-	@NotNull
 	private Long userId;
-
-	@Column(name = "refresh_token", length = 256)
-	@NotNull
-	@Size(max = 256)
 	private String refreshToken;
 
 	public RefreshToken(
