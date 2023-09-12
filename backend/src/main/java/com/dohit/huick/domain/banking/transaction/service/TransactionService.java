1c290699e4e32d6c437a8197dfaf730cd30f2a77
fluffymn <mineezm9@gmail.com>
2023-09-12 18:01:26 +0900

    ✨ Feat: Implement a service method that get transaction data by transaction id

--- backend/src/main/java/com/dohit/huick/domain/banking/transaction/service/TransactionService.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/transaction/service/TransactionService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dohit.huick.domain.banking.transaction.dto.TransactionDto;
 import com.dohit.huick.domain.banking.transaction.entity.Transaction;
 import com.dohit.huick.domain.banking.transaction.repository.TransactionRepository;
+import com.dohit.huick.global.error.ErrorCode;
+import com.dohit.huick.global.error.exception.BankingException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,5 +28,10 @@ public class TransactionService {
 
 	public List<TransactionDto> getTransactionsByUserId(String accountNumber) {
 		return transactionRepository.findBySenderAccountNumberIsOrReceiverAccountNumberIs(accountNumber, accountNumber).stream().map(TransactionDto::from).collect(Collectors.toList());
+	}
+
+	public TransactionDto getTransactionByTransactionId(Long transactionId) {
+		return TransactionDto.from(transactionRepository.findByTransactionId(transactionId).orElseThrow(() ->
+			new BankingException(ErrorCode.NOT_EXIST_TRANSACTION)));
 	}
 }
