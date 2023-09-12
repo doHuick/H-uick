9bc54d3311f5946ead2663b2462133ab3e3f0416
fluffymn <mineezm9@gmail.com>
2023-09-12 17:58:22 +0900

    ✨ Feat: Implement a query method that find transaction data by transation id

--- backend/src/main/java/com/dohit/huick/domain/banking/transaction/repository/TransactionRepository.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/transaction/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.dohit.huick.domain.banking.transaction.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import com.dohit.huick.domain.banking.transaction.entity.Transaction;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 	List<Transaction> findBySenderAccountNumberIsOrReceiverAccountNumberIs(String senderAccountNumber,
 		String receiverAccountNumber);
+
+	Optional<Transaction> findByTransactionId(Long transactionId);
 }
