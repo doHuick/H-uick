8cd795282e8bdb53cb938bab181694a120d18cbb
fluffymn <mineezm9@gmail.com>
2023-09-12 13:35:54 +0900

    ✨ Feat: Implement a response object of contract api

--- backend/src/main/java/com/dohit/huick/api/contract/dto/ContractApiDto.java
+++ backend/src/main/java/com/dohit/huick/api/contract/dto/ContractApiDto.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import com.dohit.huick.domain.contract.constant.ContractStatus;
 import com.dohit.huick.domain.contract.constant.IntervalUnit;
+import com.dohit.huick.domain.contract.dto.ContractDto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -45,4 +46,54 @@ public class ContractApiDto {
 		}
 	}
 
+	@Getter
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public static class Response {
+		Long contractId;
+		Long lesseeId;
+		Long lessorId;
+		LocalDateTime startDate;
+		LocalDateTime dueDate;
+		Integer interval;
+		IntervalUnit intervalUnit;
+		Long amount;
+		Long repaymentAmount;
+		Float rate;
+		ContractStatus status;
+		LocalDateTime createdTime;
+
+		@Builder
+		private Response(Long contractId, Long lesseeId, Long lessorId, LocalDateTime startDate, LocalDateTime dueDate,
+			Integer interval, IntervalUnit intervalUnit, Long amount, Long repaymentAmount,Float rate, ContractStatus status, LocalDateTime createdTime) {
+			this.contractId = contractId;
+			this.lesseeId = lesseeId;
+			this.lessorId = lessorId;
+			this.startDate = startDate;
+			this.dueDate = dueDate;
+			this.interval = interval;
+			this.intervalUnit = intervalUnit;
+			this.amount = amount;
+			this.repaymentAmount = repaymentAmount;
+			this.rate = rate;
+			this.status = status;
+			this.createdTime = createdTime;
+		}
+
+		public static Response from(ContractDto contractDto) {
+			return Response.builder()
+				.contractId(contractDto.getContractId())
+				.lesseeId(contractDto.getLesseeId())
+				.lessorId(contractDto.getLessorId())
+				.startDate(contractDto.getStartDate())
+				.dueDate(contractDto.getDueDate())
+				.interval(contractDto.getInterval())
+				.intervalUnit(contractDto.getIntervalUnit())
+				.amount(contractDto.getAmount())
+				.repaymentAmount(contractDto.getRepaymentAmount())
+				.rate(contractDto.getRate())
+				.status(contractDto.getStatus())
+				.createdTime(contractDto.getCreatedTime())
+				.build();
+		}
+	}
 }
