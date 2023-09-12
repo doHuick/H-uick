26f4d62a30e1bde2bcc76df7bedb8850bda3250a
fluffymn <mineezm9@gmail.com>
2023-09-12 16:26:34 +0900

    ✨ Feat: Implement autotransfer dto

---
+++ backend/src/main/java/com/dohit/huick/domain/banking/autotransfer/dto/AutoTransferDto.java
@@ -0,0 +1,30 @@
+package com.dohit.huick.domain.banking.autotransfer.dto;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class AutoTransferDto {
+	private Long autoTransferId;
+
+	@Column(nullable = false)
+	private Long contractId;
+
+	@Column(nullable = true)
+	private LocalDateTime nextTransferDate;
+
+	@Column(nullable = false)
+	private Integer unpaidCount;
+
+	@Builder
+	private AutoTransferDto(Long autoTransferId, Long contractId, LocalDateTime nextTransferDate, Integer unpaidCount) {
+		this.autoTransferId = autoTransferId;
+		this.contractId = contractId;
+		this.nextTransferDate = nextTransferDate;
+		this.unpaidCount = unpaidCount;
+	}
+}
