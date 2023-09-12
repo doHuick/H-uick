740ea0c329f1715264c019f24472e48acbc2d79e
fluffymn <mineezm9@gmail.com>
2023-09-12 15:10:15 +0900

    ✨ Feat: Implement a service method that create contract

---
+++ backend/src/main/java/com/dohit/huick/domain/contract/service/ContractService.java
@@ -0,0 +1,28 @@
+package com.dohit.huick.domain.contract.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dohit.huick.domain.banking.autotransfer.dto.AutoTransferDto;
+import com.dohit.huick.domain.banking.autotransfer.service.AutoTransferService;
+import com.dohit.huick.domain.contract.dto.ContractDto;
+import com.dohit.huick.domain.contract.entity.Contract;
+import com.dohit.huick.domain.contract.repository.ContractRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ContractService {
+	private final ContractRepository contractRepository;
+	private AutoTransferService autoTransferService;
+
+	public void createContract(ContractDto contractDto) {
+		// 계약 생성 메서드
+		Contract contract = contractRepository.save(Contract.from(contractDto));
+		if (contractDto.getUseAutoTransfer().equals("Y")) {
+			autoTransferService.createAutoTransfer(AutoTransferDto.of(contract.getContractId(), 0));
+		}
+	}
+}
