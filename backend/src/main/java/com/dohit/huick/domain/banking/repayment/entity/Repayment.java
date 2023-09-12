e0789058931548a45d0fca282c94ab8ca1746af3
fluffymn <mineezm9@gmail.com>
2023-09-12 18:04:42 +0900

    ✨ Feat: Implement a static factory method that convert repayment dto to repayment entity

--- backend/src/main/java/com/dohit/huick/domain/banking/repayment/entity/Repayment.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/repayment/entity/Repayment.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.dohit.huick.domain.banking.repayment.dto.RepaymentDto;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -38,5 +40,14 @@ public class Repayment {
 		this.contractId = contractId;
 		this.transactionId = transactionId;
 		this.repaymentNumber = repaymentNumber;
+	}
+
+	public static Repayment from (RepaymentDto repaymentDto) {
+		return Repayment.builder()
+			.repaymentId(repaymentDto.getRepaymentId())
+			.contractId(repaymentDto.getContractId())
+			.transactionId(repaymentDto.getTransactionId())
+			.repaymentNumber(repaymentDto.getRepaymentNumber())
+			.build();
 	}
 }
