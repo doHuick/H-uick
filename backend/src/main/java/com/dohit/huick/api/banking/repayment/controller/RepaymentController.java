496acb1e1e52a0e22f30038c60fb67fcaf97a9b2
fluffymn <mineezm9@gmail.com>
2023-09-12 18:12:24 +0900

    ✨ Feat: Implement a api POST `/banking/repayment` that repay money

---
+++ backend/src/main/java/com/dohit/huick/api/banking/repayment/controller/RepaymentController.java
@@ -0,0 +1,28 @@
+package com.dohit.huick.api.banking.repayment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dohit.huick.api.banking.repayment.dto.RepaymentApiDto;
+import com.dohit.huick.domain.banking.service.BankingService;
+import com.dohit.huick.domain.contract.service.ContractService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/banking/repayment")
+@RequiredArgsConstructor
+public class RepaymentController {
+	private final BankingService bankingService;
+	private final ContractService contractService;
+
+	@PostMapping
+	public ResponseEntity<Void> repay(@RequestBody RepaymentApiDto.Request request){
+		bankingService.repay(contractService.getContractByContractId(request.getContractId()), request.getAmount());\
+
+		return ResponseEntity.ok().build();
+	}
+}
