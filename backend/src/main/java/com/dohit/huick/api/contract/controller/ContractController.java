bb2fc5d0ee2fb0acff4f8ce8ea3036cae401adf8
fluffymn <mineezm9@gmail.com>
2023-09-12 15:16:37 +0900

    ✨ Feat: Implement a api GET `/{contractId}` that get contract data by contract id

--- backend/src/main/java/com/dohit/huick/api/contract/controller/ContractController.java
+++ backend/src/main/java/com/dohit/huick/api/contract/controller/ContractController.java
@@ -1,6 +1,8 @@
 package com.dohit.huick.api.contract.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,10 @@ public class ContractController {
 	public ResponseEntity<Void> createContract(@RequestBody ContractApiDto.Request request) {
 		contractService.createContract(ContractDto.from(request));
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/{contractId}")
+	public ResponseEntity<ContractApiDto.Response> getContractByContractId(@PathVariable Long contractId) {
+		return ResponseEntity.ok().body(ContractApiDto.Response.from(contractService.getContractByContractId(contractId)));
 	}
 }
