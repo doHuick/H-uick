9b015d0917501a8ca537710e3d76705993b98c36
fluffymn <mineezm9@gmail.com>
2023-09-12 16:19:00 +0900

    ✨ Feat: Implement a query method that find autotransfer data of today

---
+++ backend/src/main/java/com/dohit/huick/domain/banking/autotransfer/repository/AutoTransferRepository.java
@@ -0,0 +1,12 @@
+package com.dohit.huick.domain.banking.autotransfer.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dohit.huick.domain.banking.autotransfer.entity.AutoTransfer;
+
+public interface AutoTransferRepository extends JpaRepository<AutoTransfer, Long> {
+	List<AutoTransfer> findByNextTransferDateBetween(LocalDateTime startOfDay, LocalDateTime endOfDay);
+}
