c974b8a926e7da298e4563fa3fc66e3f0757f81f
채현종 <chj7785@gmail.com>
2023-09-19 13:48:20 +0900

    ✏️ Fix: Adding system prompt

--- ai/app/services/llm_service.py
+++ ai/app/services/llm_service.py
@@ -10,20 +10,34 @@ from ai.app.models.contract_info import ContractInfo
 conversation_history = defaultdict(list)
 
 # OpenAI API model 설정
-MODEL_NAME = 'gpt-4'
-TEMPERATURE = 0.8
+MODEL_NAME = 'gpt-3.5-turbo'
+TEMPERATURE = 1
 OPENAI_API_KEY = config('OPENAI_API_KEY')
 
 contracts_model = ChatOpenAI(model_name=MODEL_NAME, temperature=TEMPERATURE, openai_api_key=OPENAI_API_KEY)
 # System 프롬프트 작성
 contracts_sys = SystemMessage(content="""
-    """)
+                              Communicate with users in Korean.
+                              You are a chatbot that assists with loan contract creation.
+                              The user must answer in a set format to complete the contract.
+                              But I'll call you when a user has a question or doesn't answer in the prescribed format.
+                              Do not respond to questions that are not related to the creation of the loan contract.
+                              In your judgement, if the user hasn't answered the previous question properly, give them an explanation and a set format for it.
+                              The list of questions looks like this.
+                              1. 빌려주는 금액(loanAmount): "얼마를 빌려주시나요? ('1000원'과 같은 형태로 입력해주세요)"
+                              2. 빌려주는 날짜(borrowedDate): "어느 날짜에 돈을 빌려주시나요? ('YYYYMMDD' 형식으로 입력해주세요)"
+                              3. 이자율(interestRate): "이자율은 몇 %로 하시겠습니까? ('3%'와 같은 형태로 입력해주세요)"
+                              4. 받을 날짜(maturityDate): "만기일은 언제로 할까요? ('YYYYMMDD' 형식으로 입력해주세요)"
+                              5. 특약사항(specialAgreements): "이외에 추가로 정하실 사항이 있으신가요? ('예 혹은 아니오'로 입력해주세요.)"
+                                (예) 5-1. 특약사항을 입력해주세요.
+                                (아니오) 5-2. 계약을 완료하겠습니다.                              
+                              """)
 
 # 계약보조 챗봇
 # 나중에 user_id 토큰에서 가져오기
 def get_assist_chat_response(user_id: int, contract_tmp_key: str, contract_info: ContractInfo, chat: str) -> str:
     chat_template = create_contract_info_template(contract_info)
-    chat_template += create_chat_template(user_id, contract_tmp_key)
+    chat_template += create_chat_template(contract_info, chat)
 
     # 메모리에 기존 대화가 저장되어 있으면 chat 더해주기
     if exist_conversations:
@@ -43,12 +57,15 @@ def get_assist_chat_response(user_id: int, contract_tmp_key: str, contract_info:
 # ContractInfo를 Human 프롬프트로 만들기
 def create_contract_info_template(contract_info: ContractInfo):
     contract_info_template = f"""
-        본인은 {contract_info.borrowedDate}에 {contract_info.loanAmount}원을 차용하였습니다.
-        이자율은 연 {contract_info.interestRate}%이고 갚을 날짜는 {contract_info.maturityDate}입니다.
-
-        특약사항
-        {contract_info.specialAgreement or '없음'}
+    1. "얼마를 빌려주시나요? ('1000원'과 같은 형태로 입력해주세요)" {contract_info.loanAmount}원
+    2. "어느 날짜에 돈을 빌려주시나요? ('YYYYMMDD' 형식으로 입력해주세요)" {contract_info.borrowedDate}
+    3. "이자율은 몇 %로 하시겠습니까? ('3%'와 같은 형태로 입력해주세요)" 연 {contract_info.interestRate}%
+    4. 받을 날짜(maturityDate): "만기일은 언제로 할까요? ('YYYYMMDD' 형식으로 입력해주세요)" {contract_info.maturityDate}
+    5. 특약사항(specialAgreements): "이외에 추가로 정하실 사항이 있으신가요? ('예 혹은 아니오'로 입력해주세요.)"
+    (예) 5-1. 특약사항을 입력해주세요. {contract_info.specialAgreement}
+    (아니오) 5-2. 계약을 완료하겠습니다. 계약
     """
+
     return contract_info_template
 
 # 유저 chat을 Human 프롬프트로 만들기
@@ -57,6 +74,7 @@ def create_chat_template(contract_info: ContractInfo, chat: str):
         위 정보는 지금까지 사용자가 입력한 계약 정보야.
         참고해서 '{chat}'에 대한 대답을 해줘
     """
+
     return chat_template
 
 # 이전 대화 내용을 Human 프롬프트로 만들기
@@ -87,10 +105,12 @@ def save_history(user_id: int, contract_tmp_key: int, conversation: dict):
 # (Update 예정) 저장된 대화내용이 있는지 확인하기
 def exist_conversations(user_id: int, contract_tmp_key: str) -> bool:
     history_key = make_history_key(user_id, contract_tmp_key)
+
     return history_key in conversation_history
 
 # (Update 예정) 저장된 대화내용 가져오기
 def get_conversations(user_id: int, contract_tmp_key: str, limit: int) -> str:
     history_key = make_history_key(user_id, contract_tmp_key)
     conversations = conversation_history[history_key][-limit:] # 최근 {limit}개의 대화 기록만 가져옴
+
     return conversations
