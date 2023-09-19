2a9bf1ebc6cdc5a9d42f307d892c35e071f6acc2
fluffymn <mineezm9@gmail.com>
2023-09-19 10:44:06 +0900

    ✨ Feat: Implement api POST `/notification/token` that create device token

--- backend/src/main/java/com/dohit/huick/api/notification/controller/NotificationController.java
+++ backend/src/main/java/com/dohit/huick/api/notification/controller/NotificationController.java
@@ -1,8 +1,11 @@
 package com.dohit.huick.api.notification.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dohit.huick.domain.notification.dto.DeviceTokenDto;
 import com.dohit.huick.domain.notification.service.NotificationService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,4 +16,9 @@ import lombok.RequiredArgsConstructor;
 public class NotificationController {
 	private final NotificationService notificationService;
 
+	@PostMapping("/token")
+	public ResponseEntity<Void> createDeviceToken(Long userId, String deviceToken) {
+		notificationService.createDeviceToken(DeviceTokenDto.of(userId, deviceToken));
+		return ResponseEntity.ok().build();
+	}
 }
