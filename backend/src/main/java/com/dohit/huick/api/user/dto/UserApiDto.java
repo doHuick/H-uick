642e61871f2d83e8fd75f982a69362027efb6093
Lee-Jiseung <ljss98@naver.com>
2023-09-19 09:06:14 +0900

    🔥 Remove: Remove unnecessary annotations in UserApiDto

--- backend/src/main/java/com/dohit/huick/api/user/dto/UserApiDto.java
+++ backend/src/main/java/com/dohit/huick/api/user/dto/UserApiDto.java
@@ -1,17 +1,10 @@
 package com.dohit.huick.api.user.dto;
 
-import org.springframework.stereotype.Component;
-
-import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
 
-@Component
 public class UserApiDto {
 
 	@Getter
-	@ToString
-	@Builder
 	public static class Request {
 
 		private Long userId;
