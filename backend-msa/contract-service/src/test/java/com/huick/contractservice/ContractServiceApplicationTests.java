aa9b2970a54b62278eb9dd0d4e3297610169a900
Lee-Jiseung <ljss98@naver.com>
2023-09-19 13:51:07 +0900

    🎉 Init: Initialize contract service project
    Co-authored-by: 박민아 <mineezm9@gmail.com>

---
+++ backend-msa/contract-service/src/test/java/com/huick/contractservice/ContractServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huick.contractservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
