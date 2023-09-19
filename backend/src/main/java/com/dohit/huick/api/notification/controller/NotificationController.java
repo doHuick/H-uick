4a4be71f994e257f8e5fee066b968a0015fa4f60
fluffymn <mineezm9@gmail.com>
2023-09-19 10:43:07 +0900

    🔥 Remove: Remove test api

--- backend/src/main/java/com/dohit/huick/api/notification/controller/NotificationController.java
+++ backend/src/main/java/com/dohit/huick/api/notification/controller/NotificationController.java
@@ -1,15 +1,9 @@
 package com.dohit.huick.api.notification.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dohit.huick.api.notification.dto.NotificationApiDto;
-import com.dohit.huick.domain.notification.dto.NotificationDto;
 import com.dohit.huick.domain.notification.service.NotificationService;
-import com.google.firebase.messaging.FirebaseMessagingException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,10 +13,4 @@ import lombok.RequiredArgsConstructor;
 public class NotificationController {
 	private final NotificationService notificationService;
 
-	@PostMapping
-	public ResponseEntity<Void> sendMessage(@RequestBody NotificationApiDto.Request request) throws
-		FirebaseMessagingException {
-		notificationService.sendNotificationByToken(NotificationDto.from(request) );
-		return ResponseEntity.ok().build();
-	}
 }
