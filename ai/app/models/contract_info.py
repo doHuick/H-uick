88ec11476f671b031d16cbfa42f5b4315482644d
채현종 <chj7785@gmail.com>
2023-09-19 13:47:13 +0900

    ✏️ Fix: Modify ContractInfo to default to none

--- ai/app/models/contract_info.py
+++ ai/app/models/contract_info.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ContractInfo(BaseModel):
-    loanAmount: int  # 빌린 금액
-    borrowedDate: str  # 빌린 날짜
-    interestRate: float  # 이자율
-    maturityDate: Optional[str]  # 갚을 날짜
-    specialAgreement: Optional[str]  # 특약사항(선택)
+    loanAmount: Optional[int] = None  # 빌린 금액
+    borrowedDate: Optional[str] = None  # 빌린 날짜
+    interestRate: Optional[float] = None  # 이자율
+    maturityDate: Optional[str] = None  # 갚을 날짜
+    specialAgreement: Optional[str] = None  # 특약사항(선택)
