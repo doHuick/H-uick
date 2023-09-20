7ba7588dea6c220e5bd22c0ee151a9d9e8eebedd
fluffymn <mineezm9@gmail.com>
2023-09-20 14:51:49 +0900

    ✨ Feat: Implement notification api response object

--- backend/src/main/java/com/dohit/huick/api/notification/dto/NotificationApiDto.java
+++ backend/src/main/java/com/dohit/huick/api/notification/dto/NotificationApiDto.java
@@ -1,5 +1,9 @@
 package com.dohit.huick.api.notification.dto;
 
+import java.time.LocalDateTime;
+
+import com.dohit.huick.domain.notification.constant.NotificationType;
+import com.dohit.huick.domain.notification.dto.NotificationDto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -18,6 +22,37 @@ public class NotificationApiDto {
 		@Builder
 		private Request(String token) {
 			this.token = token;
+		}
+	}
+
+	@Getter
+	@NoArgsConstructor
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public static class Response {
+		Long notificationId;
+		Long userId;
+		Long contractId;
+		NotificationType notificationType;
+		LocalDateTime createdTime;
+
+		@Builder
+		private Response(Long notificationId, Long userId, Long contractId, NotificationType notificationType,
+			LocalDateTime createdTime) {
+			this.notificationId = notificationId;
+			this.userId = userId;
+			this.contractId = contractId;
+			this.notificationType = notificationType;
+			this.createdTime = createdTime;
+		}
+
+		public static Response from(NotificationDto notificationDto) {
+			return Response.builder()
+				.notificationId(notificationDto.getNotificationId())
+				.userId(notificationDto.getUserId())
+				.contractId(notificationDto.getContractId())
+				.notificationType(notificationDto.getNotificationType())
+				.createdTime(notificationDto.getCreatedTime())
+				.build();
 		}
 	}
 }
