a5ea9604dd808b51390f2039e20ae11a238e24c4
fluffymn <mineezm9@gmail.com>
2023-09-20 13:03:36 +0900

    ✨ Feat: Implement user api response obejct

--- backend/src/main/java/com/dohit/huick/api/user/dto/UserApiDto.java
+++ backend/src/main/java/com/dohit/huick/api/user/dto/UserApiDto.java
@@ -1,16 +1,80 @@
 package com.dohit.huick.api.user.dto;
 
+import java.time.LocalDateTime;
+
+import com.dohit.huick.domain.auth.constant.Role;
+import com.dohit.huick.domain.auth.constant.SocialType;
+import com.dohit.huick.domain.banking.account.dto.AccountDto;
+import com.dohit.huick.domain.user.dto.UserDto;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Builder;
 import lombok.Getter;
 
 public class UserApiDto {
 
 	@Getter
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class Request {
+		private String name;
+		private String rrn;
+		private String address;
+	}
 
+	@Getter
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public static class Response {
 		private Long userId;
 		private String name;
 		private String rrn;
 		private String address;
+		private String walletAddress;
+		private SocialType socialType;
+		private String socialId;
+		private LocalDateTime createdTime;
+		private Role role;
+		private LocalDateTime withdrawalTime;
+		private String signatureUrl;
+		private LocalDateTime issueDate;
+		private AccountDto accountInfo;
+
+		@Builder
+		private Response(Long userId, String name, String rrn, String address, String walletAddress,
+			SocialType socialType,
+			String socialId, LocalDateTime createdTime, Role role, LocalDateTime withdrawalTime, String signatureUrl,
+			LocalDateTime issueDate, AccountDto accountInfo) {
+			this.userId = userId;
+			this.name = name;
+			this.rrn = rrn;
+			this.address = address;
+			this.walletAddress = walletAddress;
+			this.socialType = socialType;
+			this.socialId = socialId;
+			this.createdTime = createdTime;
+			this.role = role;
+			this.withdrawalTime = withdrawalTime;
+			this.signatureUrl = signatureUrl;
+			this.issueDate = issueDate;
+			this.accountInfo = accountInfo;
+		}
+
+		public static Response from(UserDto userDto) {
+			return Response.builder()
+				.userId(userDto.getUserId())
+				.rrn(userDto.getRrn())
+				.address(userDto.getAddress())
+				.walletAddress(userDto.getWalletAddress())
+				.socialType(userDto.getSocialType())
+				.socialId(userDto.getSocialId())
+				.createdTime(userDto.getCreatedTime())
+				.role(userDto.getRole())
+				.withdrawalTime(userDto.getWithdrawalTime())
+				.signatureUrl(userDto.getSignatureUrl())
+				.issueDate(userDto.getIssueDate())
+				.accountInfo(userDto.getAccountInfo())
+				.build();
+		}
 	}
 
 }
