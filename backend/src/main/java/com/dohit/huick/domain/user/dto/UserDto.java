938b3c010961af4d773dad11db1f991fd4238207
fluffymn <mineezm9@gmail.com>
2023-09-20 12:46:56 +0900

    ✏️ Fix: Add fields to user dto

--- backend/src/main/java/com/dohit/huick/domain/user/dto/UserDto.java
+++ backend/src/main/java/com/dohit/huick/domain/user/dto/UserDto.java
@@ -1,6 +1,10 @@
 package com.dohit.huick.domain.user.dto;
 
+import java.time.LocalDateTime;
+
 import com.dohit.huick.api.user.dto.UserApiDto;
+import com.dohit.huick.domain.auth.constant.Role;
+import com.dohit.huick.domain.auth.constant.SocialType;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -8,17 +12,35 @@ import lombok.Getter;
 @Getter
 public class UserDto {
 
-	private final Long userId;
-	private final String name;
-	private final String rrn;
-	private final String address;
+	private Long userId;
+	private String name;
+	private String rrn;
+	private String address;
+	private String walletAddress;
+	private SocialType socialType;
+	private String socialId;
+	private LocalDateTime createdTime;
+	private Role role;
+	private LocalDateTime withdrawalTime;
+	private String signatureUrl;
+	private LocalDateTime issueDate;
 
 	@Builder
-	private UserDto(Long userId, String name, String rrn, String address) {
+	private UserDto(Long userId, String name, String rrn, String address, String walletAddress, SocialType socialType,
+		String socialId, LocalDateTime createdTime, Role role, LocalDateTime withdrawalTime, String signatureUrl,
+		LocalDateTime issueDate) {
 		this.userId = userId;
 		this.name = name;
 		this.rrn = rrn;
 		this.address = address;
+		this.walletAddress = walletAddress;
+		this.socialType = socialType;
+		this.socialId = socialId;
+		this.createdTime = createdTime;
+		this.role = role;
+		this.withdrawalTime = withdrawalTime;
+		this.signatureUrl = signatureUrl;
+		this.issueDate = issueDate;
 	}
 
 	public static UserDto of(Long userId, UserApiDto.Request request) {
