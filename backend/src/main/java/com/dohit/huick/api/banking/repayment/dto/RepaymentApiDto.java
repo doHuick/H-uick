edf185b5e39fb51b673d071046c3f05b0b61429a
fluffymn <mineezm9@gmail.com>
2023-09-20 14:32:48 +0900

    ✏️ Fix: Add annotation that create no arguments constructor and change param to json

--- backend/src/main/java/com/dohit/huick/api/banking/repayment/dto/RepaymentApiDto.java
+++ backend/src/main/java/com/dohit/huick/api/banking/repayment/dto/RepaymentApiDto.java
@@ -1,11 +1,17 @@
 package com.dohit.huick.api.banking.repayment.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class RepaymentApiDto {
 
 	@Getter
+	@NoArgsConstructor
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class Request {
 		Long contractId;
 		Long amount;
