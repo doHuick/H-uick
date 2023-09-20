1b6de223f8c6333468da554934f89c25d640f55e
fluffymn <mineezm9@gmail.com>
2023-09-20 14:34:08 +0900

    ✏️ Fix: Change parameter of api POST `/notifications/token`

--- backend/src/main/java/com/dohit/huick/api/notification/controller/NotificationController.java
+++ backend/src/main/java/com/dohit/huick/api/notification/controller/NotificationController.java
@@ -2,23 +2,26 @@ package com.dohit.huick.api.notification.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dohit.huick.api.notification.dto.NotificationApiDto;
 import com.dohit.huick.domain.notification.dto.DeviceTokenDto;
 import com.dohit.huick.domain.notification.service.NotificationService;
+import com.dohit.huick.global.userinfo.UserInfo;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/notification")
+@RequestMapping("/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
 	private final NotificationService notificationService;
 
 	@PostMapping("/token")
-	public ResponseEntity<Void> createDeviceToken(Long userId, String deviceToken) {
-		notificationService.createDeviceToken(DeviceTokenDto.of(userId, deviceToken));
+	public ResponseEntity<Void> createDeviceToken(@UserInfo Long userId, @RequestBody NotificationApiDto.Request request) {
+		notificationService.createDeviceToken(DeviceTokenDto.of(userId, request.getToken()));
 		return ResponseEntity.ok().build();
 	}
 }
