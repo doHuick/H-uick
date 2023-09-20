2cff2773394695df73d1c6b095aaa101aaeaa4bb
채현종 <chj7785@gmail.com>
2023-09-20 18:00:08 +0900

    🔥 Remove: Remove unnecessary fields from contract info

--- ai/app/models/contract_info.py
+++ ai/app/models/contract_info.py
@@ -2,8 +2,6 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ContractInfo(BaseModel):
-    loanAmount: Optional[int] = None  # 빌린 금액
-    borrowedDate: Optional[str] = None  # 빌린 날짜
+    loanAmount: Optional[int] = None  # 빌려주는 금액
     interestRate: Optional[float] = None  # 이자율
-    maturityDate: Optional[str] = None  # 갚을 날짜
-    # specialAgreement: Optional[str] = None  # 특약사항(선택)
+    maturityDate: Optional[str] = None  # 만기 날짜
