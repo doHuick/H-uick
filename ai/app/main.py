c5917ebfe14d7ece3630b09c8b74dfd5dfbe835d
채현종 <chj7785@gmail.com>
2023-09-20 18:01:47 +0900

    ✨ Feat: Implement messenger contract feature

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,15 +1,27 @@
-from fastapi import FastAPI, HTTPException
-from .services import llm_service
+from fastapi import FastAPI, UploadFile, HTTPException
+from .services import contract_assist_service, contract_messenger_service
 from ai.app.models.contract_info import ContractInfo
 
 app = FastAPI()
 
-@app.post("/chats/assistant/")
-async def request_assist_chats(user_id: int, contract_tmp_key: str, contract_info: ContractInfo, chat: str):
+@app.post("/contracts/assist")
+async def create_assist_chat(user_id: int, contract_tmp_key: str, contract_info: ContractInfo, chat: str) -> str:
     try:
-        response = llm_service.get_assist_chat_response(user_id, contract_tmp_key, contract_info, chat)
+        response = contract_assist_service.create_assist_chat_response(user_id, contract_tmp_key, contract_info, chat)
 
         return {"answer": response}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.post("/contracts/messenger")
+async def create_messanger_contract(user_id: int, contract_tmp_key: str, file: UploadFile = UploadFile(...)):
+    try:
+        # 이미지 데이터 읽기
+        image_data = await file.read()
+        response = contract_messenger_service.create_messenger_contract(user_id, contract_tmp_key, image_data) # ContractInfo
+
+        return response
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
