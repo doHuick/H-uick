59d16b85944fb7c8613956465727c9bd1a063526
Lee-Jiseung <ljss98@naver.com>
2023-09-21 14:00:26 +0900

    ✨ Feat: Implement calculating interest and next auto transfer date

--- backend/src/main/java/com/dohit/huick/domain/banking/service/AutoTransferScheduler.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/service/AutoTransferScheduler.java
@@ -1,6 +1,8 @@
 package com.dohit.huick.domain.banking.service;
 
 import java.time.LocalDateTime;
+import java.time.Year;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dohit.huick.domain.banking.autotransfer.dto.AutoTransferDto;
 import com.dohit.huick.domain.banking.transaction.dto.TransactionDto;
 import com.dohit.huick.domain.contract.constant.ContractStatus;
-import com.dohit.huick.domain.contract.constant.TermUnit;
 import com.dohit.huick.domain.contract.dto.ContractDto;
 import com.dohit.huick.domain.contract.service.ContractService;
 import com.dohit.huick.domain.notification.constant.NotificationType;
@@ -24,7 +25,7 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 @Transactional
-public class AutoTransferScheduler  {
+public class AutoTransferScheduler {
 	private final BankingService bankingService;
 	private final ContractService contractService;
 	private final NotificationService notificationService;
@@ -35,43 +36,59 @@ public class AutoTransferScheduler  {
 		List<AutoTransferDto> autoTransferDtos = bankingService.getAutoTransfersOfToday();
 		Long autoTransferId = 0L;
 		try {
-			for (int i = 0; i < autoTransferDtos.size(); i++) {
-				AutoTransferDto autoTransferDto = autoTransferDtos.get(i);
+			for (AutoTransferDto autoTransferDto : autoTransferDtos) {
 				autoTransferId = autoTransferDto.getAutoTransferId();
 				Long contractId = autoTransferDto.getContractId();
 				ContractDto contractDto = contractService.getContractByContractId(contractId);
-				String senderAccountNumber = bankingService.getAccountByUserId(contractDto.getLesseeId()).getAccountNumber();
-				String receiverAccountNumber = bankingService.getAccountByUserId(contractDto.getLessorId()).getAccountNumber();;
-				Long amount = contractDto.getRepaymentAmountPerOnce();
+				String senderAccountNumber = bankingService.getAccountByUserId(contractDto.getLesseeId())
+					.getAccountNumber();
+				String receiverAccountNumber = bankingService.getAccountByUserId(contractDto.getLessorId())
+					.getAccountNumber();
+				;
 
 				// 송금한다 -> Transaction Data 생성됨
-				Long transactionId = bankingService.transferMoney(TransactionDto.of(senderAccountNumber, receiverAccountNumber, amount));
+				Long transactionId = bankingService.transferMoney(
+					TransactionDto.of(senderAccountNumber, receiverAccountNumber, autoTransferDto.getAmount()));
 
 				// 성공하면 미납 횟수를 본다 -> 미납 횟수가 1이상이면 -1 해주기
-				if(autoTransferDto.getUnpaidCount() > 0) {
+				if (autoTransferDto.getUnpaidCount() > 0) {
 					bankingService.decreaseUnpaidCount(autoTransferId);
 				}
 
-				NotificationDto notificationDto = notificationService.createNotification(NotificationDto.of(contractDto.getLesseeId(), contractId, NotificationType.TRANSFER_SUCCESS));
+				NotificationDto notificationDto = notificationService.createNotification(
+					NotificationDto.of(contractDto.getLesseeId(), contractId, NotificationType.TRANSFER_SUCCESS));
 				notificationService.sendNotification(notificationDto);
 
 				// Repayment Data 생성하기
 				bankingService.createRepayment(contractId, transactionId);
 
-				if(bankingService.isRepaymentDone(contractDto)) {	// 상환이 끝났으면 계약 상태를 `상환 완료` 로 업데이트 한다
+				if (bankingService.isRepaymentDone(contractDto)) {    // 상환이 끝났으면 계약 상태를 `상환 완료` 로 업데이트 한다
 					contractService.updateContractStatus(contractId, ContractStatus.REPAYMENT_COMPLETED);
-				} else {	// 상환이 끝나지 않았으면 다음 상환날짜를 구해서 업데이트 한다
-					LocalDateTime nextTransferDate = autoTransferDto.getNextTransferDate();
-					if(contractDto.getTermUnit().equals(TermUnit.YEAR)) {
-						nextTransferDate = nextTransferDate.plusYears(contractDto.getTerm());
-					} else if(contractDto.getTermUnit().equals(TermUnit.MONTH)) {
-						nextTransferDate = nextTransferDate.plusMonths(contractDto.getTerm());
-					} else if(contractDto.getTermUnit().equals(TermUnit.WEEK)) {
-						nextTransferDate = nextTransferDate.plusWeeks(contractDto.getTerm());
-					} else if(contractDto.getTermUnit().equals(TermUnit.DAY)) {
-						nextTransferDate = nextTransferDate.plusDays(contractDto.getTerm());
+				} else {    // 상환이 끝나지 않았으면 다음 상환날짜를 구해서 업데이트 한다
+					LocalDateTime today = autoTransferDto.getNextTransferDate();
+					LocalDateTime nextTransferDate = today.plusMonths(1L);
+					if (nextTransferDate.isAfter(contractDto.getDueDate())) { // 다음 자동이체일은 계약 기간 이후일 때
+						nextTransferDate = contractDto.getDueDate();
 					}
-					bankingService.updateNextTransferDate(contractId, nextTransferDate);
+					long between = ChronoUnit.DAYS.between(today, nextTransferDate);
+
+					boolean isStartDateLeapYear = Year.of(today.getYear()).isLeap();
+					boolean isNextTransferDateLeapYear = Year.of(nextTransferDate.getYear()).isLeap();
+					long amount = (long)(
+						contractDto.getAmount() * (1 + contractDto.getRate()) / (isStartDateLeapYear ? 366 : 365)
+							* between);
+					if (today.getYear() != nextTransferDate.getYear()) {
+						int nextTransferDateDayOfYear = nextTransferDate.getDayOfYear();
+						amount = (long)(
+							contractDto.getAmount() * (1 + contractDto.getRate()) / (isStartDateLeapYear ? 366 : 365)
+								* (between
+								- nextTransferDateDayOfYear)
+								+ contractDto.getAmount() * (1 + contractDto.getRate()) / (isNextTransferDateLeapYear ?
+								366 : 365)
+								* nextTransferDateDayOfYear);
+					}
+
+					bankingService.updateNextTransfer(contractId, nextTransferDate, amount);
 				}
 			}
 		} catch (TransferException b) {
