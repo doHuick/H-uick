bfe36e0d6cc23e53bd9d0975e4f73363b18947f3
fluffymn <mineezm9@gmail.com>
2023-09-22 18:05:39 +0900

    ✨ Feat: Implement kafka producer service in contract service

---
+++ backend-msa/contract-service/src/main/java/com/huick/contractservice/domain/service/KafkaProducer.java
@@ -0,0 +1,34 @@
+package com.huick.contractservice.domain.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.huick.contractservice.domain.dto.AutoTransferDto;
+import com.huick.contractservice.domain.repository.ContractRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducer {
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public AutoTransferDto createAutoTransfer(String topic, AutoTransferDto autoTransferDto) {
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonInString = "";
+		try {
+			jsonInString = mapper.writeValueAsString(autoTransferDto);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		kafkaTemplate.send(topic, jsonInString);
+		log.info("Kafka Producer send data from the Contract microservice: " +  autoTransferDto);
+
+		return autoTransferDto;
+	}
+}
