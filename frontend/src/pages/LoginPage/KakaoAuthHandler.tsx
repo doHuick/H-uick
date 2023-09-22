018b139f75d66c707e1ee7862cd7966d93e4db6b
이현복 <nexusin@naver.com>
2023-09-22 17:48:56 +0900

    :sparkles: Feat: API Controller, KakaoAuthHandler

--- frontend/src/pages/LoginPage/KakaoAuthHandler.tsx
+++ frontend/src/pages/LoginPage/KakaoAuthHandler.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import Typewriter from '../../components/Loader/Loader';
 import { Main } from '../../style';
+import axios, { BASE_URL } from '../../api/apiController';
 
 const KakaoAuthHandler = () => {
   const navigate = useNavigate();
@@ -23,11 +24,19 @@ const KakaoAuthHandler = () => {
       // 로그인 성공
       console.log('로그인 성공');
 
-      // 회원가입이 된 유저면 메인페이지로 이동
-      navigate('/');
-
-      // 회원가입이 안된 유저면 회원가입 페이지로 이동
-      // navigate('/signup');
+      // [23-09-22] 로그인 및 회원가입 API 완료되면 테스트
+      // axios.get(`${BASE_URL}/user/me`, {
+      //   headers: { Authorization: localStorage.getItem('access_token') },
+      // }).then((res) => {
+      //   const data = res.data;
+      //   if (data.account_info) {
+      //     // 회원가입이 된 유저면 메인페이지로 이동
+      //     navigate('/');
+      //   } else {
+      //     // 회원가입이 안된 유저면 회원가입 페이지로 이동
+      //     navigate('/signup');
+      //   }
+      // });
     }
   });
 
