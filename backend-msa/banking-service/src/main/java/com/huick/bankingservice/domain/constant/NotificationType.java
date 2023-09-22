aa38580aef8ec207deb44e3b3a92e558a8d68ce4
Mina Park <mineezm9@gmail.com>
2023-09-23 01:19:48 +0900

    ✨ Feat: Implement notification type in banking service

---
+++ backend-msa/banking-service/src/main/java/com/huick/bankingservice/domain/constant/NotificationType.java
@@ -0,0 +1,14 @@
+package com.huick.bankingservice.domain.constant;
+
+public enum NotificationType {
+	UPCOMING_TRANSFER("이체 예정"),
+	TRANSFER_SUCCESS("이체 성공"),
+	OVERDUE("연체");
+
+
+	private final String notificationType;
+
+	NotificationType(String notificationType) {
+		this.notificationType = notificationType;
+	}
+}
