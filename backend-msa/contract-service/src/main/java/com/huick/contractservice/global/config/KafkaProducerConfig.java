02863a6667a80c852a77f17e71e1d821dcf63bc9
fluffymn <mineezm9@gmail.com>
2023-09-22 18:05:22 +0900

    ✨ Feat: Implement kafka producer config in contract service

---
+++ backend-msa/contract-service/src/main/java/com/huick/contractservice/global/config/KafkaProducerConfig.java
@@ -0,0 +1,38 @@
+package com.huick.contractservice.global.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+	@Bean
+	public ProducerFactory<String, String> producerFactory() {
+		Map<String, Object> properties = new HashMap<>();
+
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9002");
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+		return new DefaultKafkaProducerFactory<>(properties);
+	}
+
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+}
