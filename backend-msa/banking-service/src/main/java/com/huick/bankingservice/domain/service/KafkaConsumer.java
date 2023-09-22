a7782ff6040a8e08fe15f32740d1486e789b97b9
fluffymn <mineezm9@gmail.com>
2023-09-22 18:05:02 +0900

    ✨ Feat: Implement kafka consumer service in banking service

---
+++ backend-msa/banking-service/src/main/java/com/huick/bankingservice/domain/service/KafkaConsumer.java
@@ -0,0 +1,40 @@
+package com.huick.bankingservice.domain.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.huick.bankingservice.domain.account.dto.AccountDto;
+import com.huick.bankingservice.domain.account.entity.Account;
+import com.huick.bankingservice.domain.autotransfer.dto.AutoTransferDto;
+import com.huick.bankingservice.domain.autotransfer.entity.AutoTransfer;
+import com.huick.bankingservice.domain.autotransfer.repository.AutoTransferRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaConsumer {
+	private AutoTransferRepository autoTransferRepository;
+
+	@KafkaListener(topics = "create-autotransfer-topic")
+	public void createAutoTransfer(String kafkaMessage) {
+		log.info("Consumer Kafka Message : " + kafkaMessage);
+
+		ObjectMapper mapper = new ObjectMapper();
+		AutoTransferDto autoTransferDto = null;
+		try {
+			autoTransferDto = mapper.readValue(kafkaMessage, AutoTransferDto.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		if(autoTransferDto != null) {
+			autoTransferRepository.save(AutoTransfer.from(autoTransferDto));
+		}
+	}
+}
