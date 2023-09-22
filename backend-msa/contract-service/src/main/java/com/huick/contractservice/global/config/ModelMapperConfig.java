61c0fb0df9a9a19d4f2a234c7f15cc79d2a00510
fluffymn <mineezm9@gmail.com>
2023-09-22 18:04:05 +0900

    ✨ Feat: Implement model mapper config

---
+++ backend-msa/contract-service/src/main/java/com/huick/contractservice/global/config/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package com.huick.contractservice.global.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class ModelMapperConfig {
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
+}
