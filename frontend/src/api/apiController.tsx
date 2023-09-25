1ddf81db8fee11b9f0f30efbcecd1ece98ca4fbd
이현복 <nexusin@naver.com>
2023-09-25 14:42:24 +0900

    :sparkles: Feat: PWA Manifest, API Controller

--- frontend/src/api/apiController.tsx
+++ frontend/src/api/apiController.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:5173'
+export const BASE_URL = 'http://localhost:8080'
 
 const instance = axios.create({
   baseURL: BASE_URL,
