7a170a7fde52617f501911ced4e946a4199a1361
Lee-Jiseung <ljss98@naver.com>
2023-09-29 14:37:48 +0900

    :sparkles: Feat: Implement methods in RepaymentRepository to remove autotransfer

--- backend/src/main/java/com/dohit/huick/domain/banking/repayment/repository/RepaymentRepository.java
+++ backend/src/main/java/com/dohit/huick/domain/banking/repayment/repository/RepaymentRepository.java
@@ -9,6 +9,7 @@ import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 
+import com.dohit.huick.domain.banking.repayment.constant.RepaymentStatus;
 import com.dohit.huick.domain.banking.repayment.entity.Repayment;
 
 public interface RepaymentRepository extends JpaRepository<Repayment, Long> {
@@ -16,10 +17,16 @@ public interface RepaymentRepository extends JpaRepository<Repayment, Long> {
 	List<Repayment> findByContractId(Long contractId);
 
 	@Lock(LockModeType.PESSIMISTIC_READ)
-	Repayment findFirstByContractIdAndRepaymentDateGreaterThanEqualOrderByRepaymentDateAsc(Long contractId,
-		LocalDateTime currentDate);
-
-	@Lock(LockModeType.PESSIMISTIC_READ)
 	Optional<Repayment> findFirstByContractIdAndRepaymentDateAfterOrderByRepaymentDateAsc(Long contractId,
 		LocalDateTime currentTime);
+
+	@Lock(LockModeType.PESSIMISTIC_READ)
+	int countRepaymentsByContractIdAndStatus(Long contractId, RepaymentStatus repaymentStatus);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	List<Repayment> findByRepaymentDateBetween(LocalDateTime startOfDay, LocalDateTime endOfDay);
+
+	Optional<Repayment> getRepaymentByRepaymentId(Long repaymentId);
+
+	Optional<Repayment> getRepaymentsByRepaymentDateBeforeAndStatus(LocalDateTime today, RepaymentStatus status);
 }
