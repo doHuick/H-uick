a4d80f85666321841ee513cd280c4084acff924d
채현종 <chj7785@gmail.com>
2023-10-04 14:38:09 +0900

    ✏️ Fix: Change request method to body

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi import FastAPI, HTTPException
 from .services import contract_assist_service, contract_messenger_service
-from .models.contract_info import ContractInfo
+from .models.assist_request import AssistRequest
+from .models.messenger_request import MessengerRequest
 
 app = FastAPI()
 
 @app.post("/contracts/assist")
-async def create_assist_chat(user_id: int, contract_tmp_key: str, contract_info: ContractInfo, chat: str) -> str:
+async def create_assist_chat(assist_request: AssistRequest) -> str:
     try:
-        response = contract_assist_service.create_assist_chat_response(user_id, contract_tmp_key, contract_info, chat)
+        response = contract_assist_service.create_assist_chat_response(assist_request.user_id, 
+                                                                       assist_request.contract_tmp_key,
+                                                                       assist_request.contract_info,
+                                                                       assist_request.chat)
 
         return {"answer": response}
     
@@ -15,11 +19,13 @@ async def create_assist_chat(user_id: int, contract_tmp_key: str, contract_info:
         raise HTTPException(status_code=500, detail=str(e))
     
 @app.post("/contracts/messenger")
-async def create_messanger_contract(user_id: int, contract_tmp_key: str, file: UploadFile = UploadFile(...)):
+async def create_messanger_contract(messenger_reqeust: MessengerRequest):
     try:
         # 이미지 데이터 읽기
-        image_data = await file.read()
-        response = contract_messenger_service.create_messenger_contract(user_id, contract_tmp_key, image_data) # ContractInfo
+        image_data = await messenger_reqeust.file.read()
+        response = contract_messenger_service.create_messenger_contract(messenger_reqeust.user_id,
+                                                                        messenger_reqeust.contract_tmp_key,
+                                                                        image_data)
 
         return response
     
---
+++ ai/app/models/messenger_request.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+from fastapi import UploadFile
+
+class MessengerRequest(BaseModel):
+    user_id: int
+    contract_tmp_key: str
+    file: UploadFile
