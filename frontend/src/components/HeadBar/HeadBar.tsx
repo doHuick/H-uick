92ea35d7720758b3dbff4239774b922dbf02c38f
이현복 <nexusin@naver.com>
2023-10-05 10:38:31 +0900

    :pencil2: Fix Implement Headbar Navigation

--- frontend/src/components/HeadBar/HeadBar.tsx
+++ frontend/src/components/HeadBar/HeadBar.tsx
@@ -1,5 +1,6 @@
 import { styled } from 'styled-components'
 import { ReactComponent as LeftArrow } from '../../assets/icons/left-arrow.svg'
+import { useNavigate } from 'react-router-dom';
 
 interface HeadBarProps {
   pageName: string;
@@ -7,11 +8,13 @@ interface HeadBarProps {
 }
 
 export default function HeadBar({ pageName, color }: HeadBarProps) {
+  const navigation = useNavigate();
+
   return (
     <HeadBarContainer pageName={pageName} color={color}>
         <HeadBarInner>
           <HeadBarLeft>
-            <LeftArrow />
+            <LeftArrow onClick={() => navigation('/')}/>
             <span>&nbsp;&nbsp;{pageName}</span>
           </HeadBarLeft>
           <HeadBarRight>
