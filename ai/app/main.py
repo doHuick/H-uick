31a5291973b932ea9d52a2f0bdd0f2950c8e83fe
채현종 <chj7785@gmail.com>
2023-10-05 12:00:34 +0900

    ✏️ Fix: Add CORS settings

--- ai/app/main.py
+++ ai/app/main.py
@@ -4,7 +4,22 @@ from .models.assist_request import AssistRequest
 from fastapi.middleware.cors import CORSMiddleware
 
 
+
 app = FastAPI()
+
+# CORS 미들웨어 설정
+origins = [
+    "http://localhost:5173",  # 허용하려는 출처
+    # 필요한 경우 다른 출처도 추가할 수 있습니다.
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/contracts/assist")
 async def create_assist_chat(assist_request: AssistRequest):
